0e9c97723d593c0802c497bcd468b4c9d7326010
Shadab Zafar <dufferzafar0@gmail.com>
2017-10-30 14:35:18 +0530

    created_at is a useful field in project_languages.csv

--- src/remove-columns.sh
+++ src/remove-columns.sh
@@ -5,8 +5,6 @@
 # Remove Column 2 (sha)
 xsv select '!2' -o commits_new.csv -- commits.csv
 
-# Remove Column 4 (created_at)
-xsv select '!4' -o project_languages_new.csv -- project_languages.csv
 
 # Remove Column 4 (ext_ref_id)
 xsv select '!4' -o project_membders_new.csv -- project_members.csv
