607d52ef9716925e272b79a8cb6c8a2b8616c9ee
Shadab Zafar <dufferzafar0@gmail.com>
2017-10-30 16:33:22 +0530

    Misc

--- src/remove-columns.sh
+++ src/remove-columns.sh
@@ -5,6 +5,13 @@
 # Remove Column 2 (sha)
 xsv select '!2' -o commits_new.csv -- commits.csv
 
+# Is this faster?
+# xsv select '!2' -- commits.csv > commits_new.csv
 
 # Remove Column 4 (ext_ref_id)
 xsv select '!4' -o project_membders_new.csv -- project_members.csv
+
+# Remove 'https://api.github.com/repos/' from projects.csv
+# 15% reduction
+# xsv select '!4' -- projects.csv | sed 's,https://api.github.com/repos/,,' > projects.csv
+sed 's,https://api.github.com/repos/,,' projects.csv | csvcut -H -p "\\" -q '"' -C 4 >
