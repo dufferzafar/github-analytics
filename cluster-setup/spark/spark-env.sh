7e3b8debaea026926c9f9ccd05d66cc37a9a5fc9
Shadab Zafar <dufferzafar0@gmail.com>
2017-11-07 20:05:22 +0530

    Add spark related configuration

---
+++ cluster-setup/spark/spark-env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export PYTHONHASHSEED=0
+
+export SPARK_LOCAL_DIRS=~/spark/local_dirs
+export HADOOP_CONF_DIR=~/hadoop/etc/hadoop
+
+export SPARK_WORKER_DIR=~/spark/worker_dirs
+
+export PYSPARK_PYTHON=python3
+export PYSPARK_DRIVER_PYTHON=python3
