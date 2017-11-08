823fc80e075f8258a027f13384d83645642665cc
Harish <hthuwal@gmail.com>
2017-11-08 13:13:00 +0530

    Spark env : use anaconda python and jupyter-lab

--- cluster-setup/spark/spark-env.sh
+++ cluster-setup/spark/spark-env.sh
@@ -7,5 +7,5 @@ export HADOOP_CONF_DIR=~/hadoop/etc/hadoop
 
 export SPARK_WORKER_DIR=~/spark/worker_dirs
 
-export PYSPARK_PYTHON=python3
-export PYSPARK_DRIVER_PYTHON=python3
+export PYSPARK_PYTHON=~/anaconda3/bin/python
+export PYSPARK_DRIVER_PYTHON=~/anaconda3/bin/jupyter-lab
