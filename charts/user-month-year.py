e0ae6c223a8a64d3b13adce05c676b337e95cae9
Harish <hthuwal@gmail.com>
2017-11-11 14:22:54 +0530

    Improvements in user-month-py

--- charts/user-month-year.py
+++ charts/user-month-year.py
@@ -12,39 +12,36 @@ from bokeh.models import (
     ColorBar,
 )
 from bokeh.plotting import figure
-
+monthDict = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun',
+             7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
 
 data = pd.read_json(open("outputs/user-year-month-count.json"))
-print(data)
+data = data.sort_values(['year', 'month'], ascending=[True, False])
+
 data['year'] = data['year'].astype(str)
+data['month'] = data['month'].apply(lambda x: monthDict[x])
+
+cols = ['year', 'month', 'count']
+data = data[cols]
+data.columns = ['year', 'month', 'users']
 data = data.set_index('year')
 
-data = data.pivot_table(values='count', index=data.index, columns='month', aggfunc='first')
+# print(data)
+years = list(data.index.unique())
+months = list(monthDict.values())
+# print(months)
 
-print(data)
-# # print(data)
-# data.drop('Annual', axis=1, inplace=True)
-data.columns.name = 'Month'
-print(data)
-years = list(data.index)
-months = list(data.columns)
-
-# reshape to 1D array or rates with a month and year for each row.
-df = pd.DataFrame(data.stack(), columns=['users']).reset_index()
-print(df)
-# this is the colormap from the original NYTimes plot
-colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
 colors = Spectral11
-mapper = LinearColorMapper(palette=colors, low=df.users.min(), high=df.users.max())
-
-source = ColumnDataSource(df)
+mapper = LinearColorMapper(
+    palette=colors, low=data.users.min(), high=data.users.max())
+source = ColumnDataSource(data)
 
 TOOLS = "hover,save,pan,box_zoom,reset,wheel_zoom"
 
 p = figure(title="Github User Acquisition({0} - {1})".format(years[0], years[-1]),
-          x_range=years, y_range=list(reversed(list(map(str, months)))),
-          x_axis_location="above", plot_width=900, plot_height=400,
-          tools=TOOLS, toolbar_location='below')
+           x_range=years, y_range=list(reversed(list(map(str, months)))),
+           x_axis_location="above", plot_width=900, plot_height=400,
+           tools=TOOLS, toolbar_location='below')
 
 p.grid.grid_line_color = None
 p.axis.axis_line_color = None
@@ -53,7 +50,7 @@ p.axis.major_label_text_font_size = "5pt"
 p.axis.major_label_standoff = 0
 p.xaxis.major_label_orientation = pi / 3
 
-p.rect(x="year", y="Month", width=1, height=1,
+p.rect(x="year", y="month", width=1, height=1,
        source=source,
        fill_color={'field': 'users', 'transform': mapper},
        line_color=None)
@@ -66,8 +63,8 @@ color_bar = ColorBar(color_mapper=mapper, major_label_text_font_size="5pt",
 p.add_layout(color_bar, 'right')
 
 p.select_one(HoverTool).tooltips = [
-     ('date', '@Month @year'),
-     ('count', '@users'),
+    ('date', '@month @year'),
+    ('count', '@users'),
 ]
 
 show(p)      # show the plot
