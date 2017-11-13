b1d43276b02171649c0dd3fb4b99c3947d89ac40
Harish <hthuwal@gmail.com>
2017-11-13 21:12:40 +0530

    Language code size comparison by year

---
+++ charts/language_code_size_per_year_comparison.py
@@ -0,0 +1,70 @@
+from bokeh.io import show
+from bokeh.core.properties import value
+from bokeh.palettes import magma
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, FactorRange, HoverTool
+from bokeh.transform import factor_cmap, dodge
+from math import pi, ceil
+
+import pandas as pd
+
+odf = pd.read_json(open("outputs/top_lang_year.json"))
+odf['sum_MB'] = odf['sum_MB'].apply(lambda x: ceil(x / 1024))
+odf.rename(columns={'sum_MB': 'sum_GB'}, inplace=True)
+
+excluded = ["html", "css", "makefile", "batchfile", "cmake", "jupyter notebook", "apacheconf", "tex", "viml"]
+
+odf = odf.loc[~ (odf.language.isin(excluded))]
+data_2015 = odf.loc[odf.year == 2015].sort_values("sum_GB").tail(10).sort_values("language")
+data_2016 = odf.loc[odf.year == 2016].sort_values("sum_GB").tail(10).sort_values("language")
+data_2017 = odf.loc[odf.year == 2017].sort_values("sum_GB").tail(10).sort_values("language")
+
+
+languages = data_2015.language.unique()
+years = ['2015', '2016', '2017']
+
+
+data = {
+    'language': languages,
+    '2015': list(data_2015.sum_GB),
+    '2016': list(data_2016.sum_GB),
+    '2017': list(data_2017.sum_GB),
+}
+
+
+x = [(language, year) for language in languages for year in years]
+counts = sum(zip(data['2015'], data['2016'], data['2017']), ())
+TOOLS = "hover,save,pan,box_zoom,reset,wheel_zoom"
+source = ColumnDataSource(data=data)
+
+p = figure(
+    x_range=languages,
+    plot_height=500,
+    plot_width=1500,
+    title="Language Code Size by Year",
+    x_axis_label="Code Size (GB)"
+)
+
+p.vbar(x=dodge('language', -0.25, range=p.x_range), top='2015', width=0.2, source=source,
+       color="#c9d9d3", legend=value('2015'))
+
+p.vbar(x=dodge('language', 0.0, range=p.x_range), top='2016', width=0.2, source=source,
+       color="#718dbf", legend=value("2016"))
+
+p.vbar(x=dodge('language', 0.25, range=p.x_range), top='2017', width=0.2, source=source,
+       color="#e84d60", legend=value("2017"))
+
+hover = HoverTool(tooltips=[
+    ("users", "$y{(0.00 a)}")
+])
+
+p.y_range.start = 0
+p.x_range.range_padding = 0.1
+p.xgrid.grid_line_color = None
+
+p.yaxis.axis_label_text_font_size = "14pt"
+p.xaxis.axis_label_text_font_size = "14pt"
+p.axis.major_label_text_font_size = "15pt"
+p.add_tools(hover)
+
+show(p)
