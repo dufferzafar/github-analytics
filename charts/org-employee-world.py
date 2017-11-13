02cc0b87cebe8cf63eaf85610a8dd484ce6e25fc
Harish <hthuwal@gmail.com>
2017-11-13 17:15:57 +0530

    Bar Chart Follow per employee

--- charts/org-employee-world.py
+++ charts/org-employee-world.py
@@ -43,3 +43,29 @@ p.xaxis.axis_label_text_font_size = "14pt"
 p.axis.major_label_text_font_size = "15pt"
 
 show(p)
+
+odf = odf.sort_values("followers_per_employee", ascending=False)
+s = figure(
+    x_range=odf.corporate.unique(),
+    plot_height=800,
+    plot_width=1000
+)
+
+colors = magma(30)
+colors = (colors)[6:]
+
+s.vbar(
+    source=odf,
+    x='corporate', top='followers_per_employee',
+    width=1,
+    line_color='white',
+    fill_color=factor_cmap('corporate', palette=colors,
+                           factors=odf.corporate.unique())
+)
+
+s.xaxis.major_label_orientation = pi / 3
+s.yaxis.axis_label_text_font_size = "14pt"
+s.xaxis.axis_label_text_font_size = "14pt"
+s.axis.major_label_text_font_size = "15pt"
+
+show(s)
